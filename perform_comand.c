794224858f5ac0f318ce80e7c285df82bf49bbe8
Austine <olochom@gmail.com>
2023-11-17 22:08:12 +0300

    updates recent

--- perform_comand.c
+++ perform_comand.c
@@ -11,36 +11,24 @@
  */
 void execute_instruction(const char *instruction)
 {
-	pid_t child_process = fork();
 
-	if (child_process == -1)
+	pid_t child_pid = fork();
+
+	if (child_pid == -1)
 	{
 		perror("fork");
 		exit(EXIT_FAILURE);
 	}
-	else if (child_process == 0)
+	else if (child_pid == 0)
 	{
-		char *message[MAX_ARGUMENTS];
-		int message_count = 0;
 
-		char *token = strtok((char *)instruction, " ");
-
-		while (token != NULL && message_count < MAX_ARGUMENTS - 1)
-		{
-			message[message_count++] = token;
-			token = strtok(NULL, " ");
-		}
-		message[message_count] = NULL;
-
-		if (execve(message[0], message, NULL) == -1)
-		{
-			perror("execve");
-
-			exit(EXIT_FAILURE);
-		}
+		execlp(instruction, instruction, (char *)NULL);
+		perror("execlp");
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
+
 		wait(NULL);
 	}
 }
