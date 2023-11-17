f67ad82b33d57e61ed99e22d3a6ccc8037e57712
Zippymimo <zippywairimo@gmail.com>
2023-11-17 22:38:31 +0300

    recents

--- perform_comand.c
+++ perform_comand.c
@@ -8,7 +8,7 @@
  * This function takes a command instruction as input, forks a cp,
  * and executes the instruction in the child process using the execve system
  * call. It waits for the child process to complete in the parent process.
-
+*/
 void execute_instruction(const char *instruction)
 {
 
@@ -30,32 +30,5 @@ void execute_instruction(const char *instruction)
 	{
 
 		wait(NULL);
-	}
-}
-*/
-void execute_instruction(const char *instruction)
-{
-    pid_t child_pid = fork();
-
-    if (child_pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (child_pid == 0) {
-        char *args[MAX_ARGUMENTS];
-        int arg_count = 0;
-
-        char *token = strtok((char *)instruction, " ");
-        while (token != NULL && arg_count < MAX_ARGUMENTS - 1) {
-            args[arg_count++] = token;
-            token = strtok(NULL, " ");
-        }
-        args[arg_count] = NULL;
-
-        execvp(args[0], args);
-
-        perror("execvp");
-        exit(EXIT_FAILURE);
-    } else {
-        wait(NULL);
 	}
 }
